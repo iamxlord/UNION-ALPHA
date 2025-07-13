8759aebf0ddcf2e722c5529d969a605bc84b056e
iamxlord <yourssincerelyiamxlord@gmail.com>
2025-07-13 11:56:13 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
- vite.config.js
 import { defineConfig } from 'vite';
-import react from '@vitejsplugin-react';
+import react from '@vitejs/plugin-react';
 
- httpsvitejs.devconfig
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins [react()],
-  server {
-    host '0.0.0.0',
-    port 5173,
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
   },
 });
